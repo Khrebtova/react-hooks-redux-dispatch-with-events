e2ba645fb5776674f48a327b4e06f37836572b31
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-07-05 15:20:39 -0700

    commit tree ff120c4e49652919d7b2c61c87df87bc06cdfeb3

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,27 @@
 // add code snippets from README
+let state;
+
+const reducer = (state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'counter/increment' :
+            return {count: state.count + 1};
+        default:
+            return state;
+    }
+}
+const render =()=>{
+    const container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+const dispatch = (action) => {
+    state = reducer(state, action);
+    render();
+}
+
+const button = document.getElementById('button');
+button.addEventListener('click', () => {
+    dispatch({type: 'counter/increment'});
+} );
+
+dispatch({ type: "@@INIT" });
